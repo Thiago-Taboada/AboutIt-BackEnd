c6cbc24452714c3b650851e7eddf6d5aa2661003
Thiago-Taboada <contact.thiago.taboada@gmail.com>
2022-07-08 14:17:08 +0000

    rename paths

--- src/routers/comunidadeRouter.js
+++ src/routers/comunidadeRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const Comunidade = require('../../models/aboutit/comunidade')
+const Comunidade = require('../models/aboutIt/comunidade')
 
 router.get('/', async (req, res) => {
     const comunidades = await Comunidade.findAll();
